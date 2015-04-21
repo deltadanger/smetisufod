d2b2f1c4a52588a116759a7516736db38d28dfe0
Thierry Jacoel <thierry.jacoel@gmail.com>
2015-04-21 16:28:06 +1000

    Added automated management of credentials

--- mainsite/management/commands/pictures.py
+++ mainsite/management/commands/pictures.py
@@ -8,15 +8,18 @@ from googleapiclient.http import MediaFileUpload
 import urllib2
 from smetisufod.settings import DOFUS_COOKIES
 from django.core.files.temp import NamedTemporaryFile
+from oauth2client.file import Storage
 
 
 # Check https://developers.google.com/drive/scopes for all available scopes
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+CREDENTIAL_FILE = "credentials"
 
 FOLDER_ID = "0B7K23HtYjKyBfnhYbkVyUld3YUVqSWgzWm1uMXdrMzQ0NlEwOXVUd3o0MWVYQ1ZVMlFSNms"
 DRIVE_FOLDER_LINK = "http://googledrive.com/host/{folder_id}/{file_name}"
 
 def upload_image_file(dofus_url):
+    # Download image from dofus static into a temporary file
     opener = urllib2.build_opener()
     opener.addheaders.append(('Cookie', DOFUS_COOKIES))
     
@@ -31,20 +34,26 @@ def upload_image_file(dofus_url):
         temp_file.write(buff)
         buff = resp.read(BUFFER_SIZE)
     
-    # TODO: manage credentials automatically
-    # Run through the OAuth flow and retrieve credentials
-    flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, redirect_uri=REDIRECT_URI)
-    authorize_url = flow.step1_get_authorize_url()
-    print 'Go to the following link in your browser: ' + authorize_url
-    code = raw_input('Enter verification code: ').strip()
-    credentials = flow.step2_exchange(code)
+    # Get stored credentials
+    storage = Storage(CREDENTIAL_FILE)
+    credentials = storage.get()
+    if not credentials:
+        # Run through the OAuth flow and retrieve credentials
+        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, redirect_uri=REDIRECT_URI)
+        authorize_url = flow.step1_get_authorize_url()
+        print 'Go to the following link in your browser: ' + authorize_url
+        code = raw_input('Enter verification code: ').strip()
+        credentials = flow.step2_exchange(code)
+        storage.put(credentials)
     
     # Create an httplib2.Http object and authorize it with our credentials
     http = httplib2.Http()
     http = credentials.authorize(http)
     
+    # Build the drive API service object
     service = build('drive', 'v2', http=http)
     
+    # Upload the file to GDrive
     mime_type = "image/png"
     media_body = MediaFileUpload(temp_file.name, mimetype=mime_type)
     body = {
@@ -54,7 +63,7 @@ def upload_image_file(dofus_url):
     }
     
     service.files().insert(body=body, media_body=media_body).execute()
-    temp_file.close()
+    temp_file.close() # Close also destroys the temp file
     
     return DRIVE_FOLDER_LINK.format(folder_id=FOLDER_ID, file_name=file_name)
 
